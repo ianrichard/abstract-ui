2c1134f712894c72050bc58b1257341a417e22e8
Ian Richard <smith.ianrichard@gmail.com>
2022-06-28 17:21:17 +0000

    support for gitpod

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const config = require('./webpack.config.js');
 
+const port = 3000;
+let publicUrl = `ws://localhost:${port}/ws`;
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+  publicUrl = `${port}-${host}`;
+}
+
 config.devServer = {
-    historyApiFallback: true,
-    hot: true,
-    contentBase: path.join(__dirname, '../build'),
-    port: 8080,
+  historyApiFallback: true,
+  hot: true,
+  contentBase: path.join(__dirname, '../build'),
+  port: port,
+  allowedHosts: ['.gitpod.io'],
+  sockHost: publicUrl,
+  sockPort: 443,
+  sockPath: '',
 };
 
 config.devtool = 'inline-source-map';
