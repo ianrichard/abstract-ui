1de727cde0534f2ec7dd0c72487ef14ed1b1ffcd
Ian Richard <smith.ianrichard@gmail.com>
2022-07-02 19:11:14 +0000

    fix gitpod config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -10,14 +10,16 @@ config.devServer = {
 };
 
 if (process.env.GITPOD_WORKSPACE_URL) {
-    const [host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+    const workspaceUrl = process.env.GITPOD_WORKSPACE_URL;
+    const demoUrl = `https://${port}-${workspaceUrl.split('://')[1]}`;
     config.devServer = {
         ...config.devServer,
         allowedHosts: ['.gitpod.io'],
-        sockHost:  `${port}-${host}`,
+        sockHost:  `${port}-${workspaceUrl.split('://')[1]}`,
         sockPort: 443,
         sockPath: '',
     }
+    console.log(`project is running at ${demoUrl}`);
 }
 
 config.devtool = 'inline-source-map';
